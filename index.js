06b105601bb6ffc7cdfcee1eac6c70e177906dc4
gtblackmore <gtblackmore@gmail.com>
2020-06-18 16:31:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(namesArray, event) {
+  let cards = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    cards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+  }
+  return cards
+}
+
+
+function countDown(startInteger){
+  while (startInteger > 0) {
+    console.log(startInteger);
+    startInteger-= 1;  
+  } 
+  console.log(startInteger);
+}
