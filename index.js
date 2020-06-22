1424e1428676bd19f87cbcdda3d4235ccc116eba
asijaw <asija.w@yahoo.com>
2020-06-22 00:20:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(nameArray, eventName) {
+    let thanks = []
+    for (let i = 0; i < nameArray.length; i++) {
+        thanks.push(`Thank you, ${nameArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thanks;
+}
+
+function countDown(int) {
+    let i = int;
+    while(i > 0) {
+        console.log(i);
+        i--;
+    }
+    console.log(i)
+}
+
