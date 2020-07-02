ff93c1f690c5b6c4e0992907734cebf58bccdb31
lkorchnoy <lkorchnoy@yahoo.com>
2020-07-02 01:33:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+
+
+function writeCards(namesArray) {
+    let thankYouCards = []
+    for (let i = 0; i < namesArray.length; i++) {
+        thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYouCards
+}
+
+
+
+
+
+
+function countDown(counter) {
+    while (counter > 0) {
+        console.log(counter);
+        counter -= 1;
+    }
+    console.log(counter);
+}
